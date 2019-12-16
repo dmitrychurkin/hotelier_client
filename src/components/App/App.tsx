37c63d4c3301f4cd692e13116c4843809f6288a9
dmitry <dmitrychurkin1988@gmail.com>
2019-12-16 07:01:01 +0200

    initial commit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+import "./App.css";
+
+const USER_BY_ID = gql`
+  query UserByID($id: ID!) {
+    userById(id: $id) {
+      id
+      email
+    }
+  }
+`;
 
 const App: React.FC = () => {
+  const query = useQuery(USER_BY_ID, {
+    context: {
+      testContext: true
+    },
+    variables: { id: "ck4332g32ly0x0922q6y11ask" }
+  });
+  console.log("query => ", query);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -21,6 +37,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink, from } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import App from "components/App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/query"
+});
+const middlewareLink = new ApolloLink((operation, forward) => {
+  // console.log("befowareLink => ", args);
+  // const [operation, forward] = args;
+  console.log("operation => ", operation);
+  const beforeReqCtx = operation.getContext();
+  console.log("beforeReqCtx => ", beforeReqCtx);
+  operation.setContext((args: any) => {
+    console.log("setContextArgs => ", args);
+    const { headers = {} } = args;
+    return {
+      headers: {
+        ...headers,
+        "x-test-context": beforeReqCtx.testContext
+      }
+    };
+  });
+
+  return forward(operation).map(response => {
+    console.log("response => ", response);
+    const context = operation.getContext();
+    console.log("context => ", context);
+    const {
+      response: { headers }
+    } = context;
+    console.log("headers => ", headers.get("x-refresh-token"));
+    return response;
+  });
+});
+const client = new ApolloClient({
+  link: from([middlewareLink, httpLink]),
+  cache: new InMemoryCache()
+});
+const root = document.getElementById("root");
+if (root) {
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    root
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
